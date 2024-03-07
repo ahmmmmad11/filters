b01d1c3b4ada396fb75c7465c1e7b147ac868810
Ahmed Mohamed <alamerahmed00@gmail.com>
2024-03-07 11:13:25 +0400

    edit config comment

--- config/filters.php
+++ config/filters.php
@@ -7,7 +7,7 @@ return [
     | Filters Path
     |--------------------------------------------------------------------------
     |
-    | This value is the path where you filter class be created.
+    | This value is the path where your filter class will be created.
     |
     */
 
